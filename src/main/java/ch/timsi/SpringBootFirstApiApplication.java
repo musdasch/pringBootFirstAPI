b9ddc7b5539d54669e9f91d25fa1d919ea70470b
Tim Vögtli <tim.voegtli@hf-ict.info>
2021-06-01 15:49:23 +0200

    Createt simpla spring boot projekt

---
+++ src/main/java/ch/timsi/SpringBootFirstApiApplication.java
@@ -0,0 +1,13 @@
+package ch.timsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/timsi/SpringBootFirstApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.timsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
